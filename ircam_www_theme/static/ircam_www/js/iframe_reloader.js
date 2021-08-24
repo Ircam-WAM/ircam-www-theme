770cfcc6b4e02277b7ca6cff0cc777db9d3fb443
Clément Gérard <clement.gerard@ircam.fr>
2021-08-24 15:39:06 +0200

    update theme struct for django auto detect app include

--- ircam_www_theme/static/ircam_www/js/iframe_reloader.js
+++
@@ -1,8 +0,0 @@
-// In the <iframe> host page
-window.addEventListener("message", function(message) {
-    if(message.data == 'reload') {
-        console.log('Reloading the iframe...');
-        var iframe = document.getElementById("shop-iframe");
-        iframe.src = iframe.src;
-    }
-}, false);
