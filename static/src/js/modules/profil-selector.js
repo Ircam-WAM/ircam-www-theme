cfaf2b4aa5563c0e7158ed7a705d36abb2343ba6
Clément Gérard <clement.gerard@ircam.fr>
2021-10-18 16:27:45 +0200

    fix login button

--- static/src/js/modules/profil-selector.js
+++ static/src/js/modules/profil-selector.js
@@ -14,16 +14,40 @@ ProfilSelector.prototype.init = function() {
 
     var that = this;
 
-    that.$element = $('#ProfilSelector');
-    that.$element.find('li:first-child a').click(function(e) {
+
+    // Desktop profil selector
+    that.$element = document.querySelector('.profilSelector:last-of-type');
+    that.$element.addEventListener("click", function(e) {
+
+        that.$element = $(that.$element)
 
         if ($(this).hasClass('unclickable')) {
             e.preventDefault();
         }
+
         that.$element.toggleClass('open');
         if(that.$element.hasClass('open')) {
             that.$element.one('mouseleave', function() {
                 that.$element.removeClass('open');
+            })
+        }
+
+    });
+
+    // Mobile profil selector
+    that.$mobElement = document.querySelector('.profilSelector:first-of-type');
+    that.$mobElement.addEventListener("click", function(e) {
+
+        that.$mobElement = $(that.$mobElement)
+
+        if ($(this).hasClass('unclickable')) {
+            e.preventDefault();
+        }
+
+        that.$mobElement.toggleClass('open');
+        if(that.$mobElement.hasClass('open')) {
+            that.$mobElement.one('mouseleave', function() {
+                that.$mobElement.removeClass('open');
             })
         }
 
