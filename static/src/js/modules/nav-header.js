1bee17d8ee71a287ada93a4da4a2c87d6146d65a
Clément Gérard <clement.gerard@ircam.fr>
2021-11-16 12:07:06 +0100

    refacto main header on computer

--- static/src/js/modules/nav-header.js
+++ static/src/js/modules/nav-header.js
@@ -1,7 +1,8 @@
 var NavHeader = function() {
 
-    this.$element = $('#navHeader');
-    this.$elements = $('#navHeader .nav-header__item');
+    this.element = document.getElementById('navHeader');
+    console.log()
+    this.elements = this.element.getElementsByClassName('nav-header__item');
 
     //
     // Init
@@ -14,25 +15,33 @@ NavHeader.prototype.init = function() {
 
     var that = this;
 
-    that.$elements.hover(function(e) {
-
-        var $submenu = $(this).find('.nav-header__submenu');
-
-        if($submenu.length > 0) {
-
-            var offsetLeft = $(this).position().left,
-                originOffsetLeft = $('#navHeader .nav-header__item:first-child').position().left + 100,
-                originWidth = that.$element.width();
-
-            $(this).toggleClass('hover');
-
-            $submenu.css('left', -offsetLeft + originOffsetLeft);
-            $submenu.css('width', originWidth - 200);
-            $submenu.toggle();
-
+    for (var e in this.elements) {
+        if (typeof this.elements[e] === "object") {
+            (function(){
+                var elem = e;
+                var submenus = that.elements[elem].getElementsByClassName('nav-header__submenu');
+                if(submenus.length > 0){
+                    var submenu = submenus[0]
+                    var submenuHover = false
+                    submenu.addEventListener("mouseover", function(){
+                        submenuHover = true
+                    })
+                    that.elements[elem].addEventListener("mouseover", function(){
+                        that.elements[elem].classList.add("hover")
+                        submenu.classList.add("visible")
+                        submenuHover = false
+                    })
+                    that.elements[elem].addEventListener("mouseout", function(){
+                        if (!submenuHover) {
+                            that.elements[elem].classList.remove("hover")
+                            submenu.classList.remove("visible")
+                        }
+                        submenuHover = false
+                    })
+                }
+            })()
         }
-
-    });
+    }
 
 };
 
